ae2bdde97c7453689aeefa283a4db26d258f932c
Kami900 <92232492+Kami900@users.noreply.github.com>
2021-10-10 03:01:23 +0500

    Create Kamrankami.py
    Pkg update
    Pkg upgrade
    Pkg install git
    Pkg install python2
    Pip2 install mechani
    Pip2 install requests
    Pip2 install lolcat
    git clone https://github.com/Kami900/kami900.git
    cd kamran900
    python2 kami.py

---
+++ Kamrankami.py
@@ -0,0 +1,16 @@
+- 👋 Hi, I’m @Kamran900
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Kamran900/Kamran900 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the 
+___====-_ _-====___ _--^^^#####// \\#####^^^--_ _-^##########// ( ) \\##########^-_ -############// |\^^/| \\############- _/############// (@::@) \\############\_ /#############(( \\// ))#############\ -###############\\ (oo) //###############- -#################\\ / VV \ //#################- -###################\\/ \//###################- _#/|##########/\######( /\ )######/\##########|\#_ |/ |#/\#/\#/\/ \#/\##\ | | /##/\#/ \/\#/\#/\#| \| ` |/ V V ` V \#\| | | |/#/ V ' V V \| ' ` ` ` ` / | | | | \ ' ' ' ' ( | | | | ) __\ | | | | /__ (vvv(VVV)(VVV)vvv)
+love is life 
+iTx kami🔥
+member of syco speed gang😜
+risk is my life
+danger is my game💋
+kamran is my name😁
